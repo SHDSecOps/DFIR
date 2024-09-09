b79afff110457d473db719114f321c97d7c043c7
SHD SecOps <141743360+SHDSecOps@users.noreply.github.com>
2024-09-09 12:02:03 +0200

    Update Get-ExecutableFiles.ps1
    update from local

--- Get-ExecutableFiles/Get-ExecutableFiles.ps1
+++ Get-ExecutableFiles/Get-ExecutableFiles.ps1
@@ -26,13 +26,18 @@
     I intentionally skipped most FileInfo, because they are not relevant for fast IR
 
     TODOs:
-    - change write methods to [+] -> its prettier!
-    - exclude param for the lazy ones
+    - excludefiletype param for the lazy ones
 .LINK
     https://www.shd-online.de/leistungsprofil/it-security/loesungen/incident-response-team/
 .EXAMPLE
-    Get-Executables -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    .\Get-Executables -Verbose
+    Outputs a list of all executable files that this script searches for. Uses current location since none is given. Also gives verbose infos while executing (very noisy)
+.EXAMPLE
+    .\Get-ExecutableFiles.ps1 -Path 'C:\Temp\' | ft -au
+    Outputs a formatted table of all executable files that this script searches for in the folder c:\temp
+.EXAMPLE
+    .\Get-Executables -path "C:\windows\system32" -filetypes ps1,exe | Export-csv -path C:\forensics\foundfiles.csv -encoding UTF8 -notypeinformation
+    Eports a csv of all .exe and .ps1 files in folder c:\windows\system32 to the folder C:\forensics\foundfiles.csv
 #>
 
 [CmdletBinding()]
@@ -70,10 +75,11 @@ foreach ($filetype in $filetypes){
     Write-Host ("Found {0} {1} Files" -f $tempitems.count, $filetype) -ForegroundColor Cyan
     
     foreach ($tempitem in $tempitems) {
+        #add column for more infos
         $tempitem | Add-Member -NotePropertyName "Description1" -NotePropertyValue ([System.Diagnostics.FileVersionInfo]::GetVersionInfo($tempitem.FullName).FileDescription)
         $tempitem | Add-Member -NotePropertyName "SHA1" -NotePropertyValue ((Get-FileHash $tempitem.FullName -Algorithm SHA1).Hash)
     }
-    Write-Verbose ($tempitems | ft Extension,Name, BaseName, Mode, Description1, CreationTime, LastWriteTime | Out-String)
+    Write-Verbose ($tempitems | ft Extension,Name, BaseName, Mode, Description1, CreationTime, LastWriteTime,DirectoryName,SHA1 | Out-String)
     if ($tempitems.count -gt 0) {
         $items += $tempitems
     }
